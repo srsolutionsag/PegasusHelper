de3bd72eb0bea369265a8fcd7dff845b1cc75dd0
Nicolas Schäfli <ns@studer-raimann.ch>
2018-01-31 11:04:11 +0100

    Increment version number

--- plugin.php
+++ plugin.php
@@ -1,6 +1,6 @@
 <?php
 $id = 'sragpegasushelper';
-$version = '0.0.10';
+$version = '0.0.11';
 $ilias_min_version = '5.1.0';
 $ilias_max_version = '5.2.999';
 $responsible = 'Stefan Wanzenried, Martin Studer, Nicolas Märchy';
