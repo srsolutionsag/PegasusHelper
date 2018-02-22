0c6be47de078037a7c87d68f65a36086643d75ae
Nicolas Schäfli <ns@studer-raimann.ch>
2018-02-22 10:48:48 +0100

    increase version constraint for pegasus helper to 5.3

--- plugin.php
+++ plugin.php
@@ -2,6 +2,6 @@
 $id = 'sragpegasushelper';
 $version = '0.0.11';
 $ilias_min_version = '5.2.0';
-$ilias_max_version = '5.2.999';
+$ilias_max_version = '5.3.999';
 $responsible = 'Martin Studer, Nicolas Märchy, Nicolas Schäfli';
 $responsible_mail = 'info@studer-raimann.ch';
