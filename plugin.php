444aa86ca8f1cb454c6ca6851ad13cb14d87a6f9
Marc Schneiter <msc@studer-raimann.ch>
2019-05-22 15:37:26 +0200

    merge changes back from master

--- plugin.php
+++ plugin.php
@@ -2,6 +2,6 @@
 $id = 'sragpegasushelper';
 $version = '1.0.1';
 $ilias_min_version = '5.2.0';
-$ilias_max_version = '5.3.999';
+$ilias_max_version = '5.4.999';
 $responsible = 'Martin Studer, Nicolas Märchy, Nicolas Schäfli';
 $responsible_mail = 'info@studer-raimann.ch';
