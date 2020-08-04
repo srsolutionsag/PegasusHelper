cd8996486ab49bed6892f8c225921126ad3285ed
Nicolas Schäfli <ns@studer-raimann.ch>
2020-08-04 13:10:13 +0200

    Update plugin responsible entry

--- plugin.php
+++ plugin.php
@@ -3,5 +3,5 @@ $id = 'sragpegasushelper';
 $version = '1.1.4';
 $ilias_min_version = '5.2.0';
 $ilias_max_version = '6.0';
-$responsible = 'Martin Studer, Nicolas Märchy, Nicolas Schäfli, Marc Schneiter, Jonathan Gerber';
+$responsible = 'Martin Studer, Nicolas Schäfli';
 $responsible_mail = 'info@studer-raimann.ch';
